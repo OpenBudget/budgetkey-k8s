486228f615d58b3af1ac177e9401387e4949b6ba
Ori Hoch <ori@uumpa.com>
2018-07-03 18:02:55 +0300

    support patching of container images using kubectl based on commit message

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -6,10 +6,20 @@ openssl aes-256-cbc -K $encrypted_93b308bc8a42_key -iv $encrypted_93b308bc8a42_i
 K8S_ENVIRONMENT_NAME="budgetkey"
 OPS_REPO_SLUG="OpenBudget/budgetkey-k8s"
 OPS_REPO_BRANCH="${TRAVIS_BRANCH}"
-./run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" "
+./run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" '
     RES=0;
     curl -L https://raw.githubusercontent.com/hasadna/hasadna-k8s/master/apps_travis_script.sh | bash /dev/stdin install_helm;
-    if ./helm_upgrade_all.sh --install --dry-run --debug; then
+    ./kubectl_patch_charts.py '"${TRAVIS_COMMIT_MESSAGE}"' --dry-run
+    PATCH_RES=$?
+    if [ "${PATCH_RES}" != "2" ]; then
+        echo detected patches based on commit message
+        if [ "${PATCH_RES}" == "0" ]; then
+            ! ./kubectl_patch_charts.py '"${TRAVIS_COMMIT_MESSAGE}"' && echo failed patches && RES=1
+            ! ./helm_healthcheck.sh && echo Failed healthcheck && RES=1
+        else
+            echo patches dry run failed && RES=1
+        fi
+    elif ./helm_upgrade_all.sh --install --dry-run --debug; then
         echo Dry run was successfull, performing upgrades
         ! ./helm_upgrade_all.sh --install && echo Failed upgrade && RES=1
         ! ./helm_healthcheck.sh && echo Failed healthcheck && RES=1
@@ -18,10 +28,10 @@ OPS_REPO_BRANCH="${TRAVIS_BRANCH}"
         RES=1
     fi
     sleep 2;
-    kubectl get pods --all-namespaces;
-    kubectl get service --all-namespaces;
-    exit "'$'"RES
-" "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "environments/budgetkey/secret-k8s-ops.json"
+    kubectl get pods;
+    kubectl get service;
+    exit $RES
+' "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "environments/budgetkey/secret-k8s-ops.json"
 if [ "$?" == "0" ]; then
     echo travis deployment success
     exit 0
