3de07de96707605a98821f505d33541ca06afc85
Ori Hoch <ori@uumpa.com>
2018-07-05 19:52:56 +0300

    Patch chart images directly using kubectl (#24)
    * add automatic update values
    
    * support patching of container images using kubectl based on commit message
    
    * fix parsing of travis commit message
    
    * move automatic-update config from values.yaml (which is exposed to helm) to charts-config.yaml

--- read_env_yaml.sh
+++ read_env_yaml.sh
@@ -2,15 +2,15 @@
 
 source connect.sh >/dev/null
 
-ENV_AUTO_VALUE="`./read_yaml.py environments/${K8S_ENVIRONMENT_NAME}/values.auto-updated.yaml "$@" 2>/dev/null`"
-if [ "${ENV_AUTO_VALUE}" != "" ] && [ "${ENV_AUTO_VALUE}" != "{}" ]; then
-    echo "${ENV_AUTO_VALUE}"
-    exit 0
-fi
-
 ENV_MAN_VALUE="`./read_yaml.py environments/${K8S_ENVIRONMENT_NAME}/values.yaml "$@" 2>/dev/null`"
 if [ "${ENV_MAN_VALUE}" != "" ] && [ "${ENV_MAN_VALUE}" != "{}" ]; then
     echo "${ENV_MAN_VALUE}"
+    exit 0
+fi
+
+ENV_AUTO_VALUE="`./read_yaml.py values.auto-updated.yaml "$@" 2>/dev/null`"
+if [ "${ENV_AUTO_VALUE}" != "" ] && [ "${ENV_AUTO_VALUE}" != "{}" ]; then
+    echo "${ENV_AUTO_VALUE}"
     exit 0
 fi
 
